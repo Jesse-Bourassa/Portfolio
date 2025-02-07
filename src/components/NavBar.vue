7dd66cb54693f9c23b8f2863f0387c08b013f4c8
TheUnnamedOne1 <jesse.bou@outlook.com>
2025-02-06 21:05:36 -0500

    test webhook

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -11,7 +11,7 @@
       <li>
         <router-link to="/Blog">
           <i class="pi pi-user"></i>
-          <span>Blog</span>
+          <span>ALLO ZacharyLelievre</span>
         </router-link>
       </li>
     </ul>
