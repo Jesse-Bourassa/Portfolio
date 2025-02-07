6cb722bafaba3ee1e76e3a75cf49c8a0adfe867c
TheUnnamedOne1 <jesse.bou@outlook.com>
2025-02-06 22:18:55 -0500

    test webhoow

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -11,7 +11,7 @@
       <li>
         <router-link to="/Blog">
           <i class="pi pi-user"></i>
-          <span>Blogss</span>
+          <span>Blog</span>
         </router-link>
       </li>
     </ul>
