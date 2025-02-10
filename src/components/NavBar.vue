b81c181b037871c51dd3fd9293588593a88441ad
TheUnnamedOne1 <jesse.bou@outlook.com>
2025-02-10 17:18:18 -0500

    trest

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -11,7 +11,7 @@
       <li>
         <router-link to="/Blog">
           <i class="pi pi-user"></i>
-          <span>Blog</span>
+          <span>Blogsss</span>
         </router-link>
       </li>
 
